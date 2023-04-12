9f2f2cd7d233954c3e33591cbcf8994446856021
CreepyPvP <interruptedThread@gmail.com>
2023-04-12 21:28:51 +0200

    I am dumb

--- lua/luis/packer.lua
+++ lua/luis/packer.lua
@@ -43,7 +43,11 @@ return require('packer').startup(function(use)
         config = function()
             require('Comment').setup()
         end
-    }
+    },
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+    end},
 
+    use( 'theprimeagen/vim-be-good' )
 }
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -161,6 +161,11 @@ _G.packer_plugins = {
     path = "C:\\Users\\inter\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\undotree",
     url = "https://github.com/mbbill/undotree"
   },
+  ["vim-be-good"] = {
+    loaded = true,
+    path = "C:\\Users\\inter\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-be-good",
+    url = "https://github.com/theprimeagen/vim-be-good"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "C:\\Users\\inter\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-fugitive",
