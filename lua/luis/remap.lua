afe0b7c1a8530880f7e549d0683bb911491fc92e
CreepyPvP <interruptedThread@gmail.com>
2023-04-20 16:21:58 +0200

    Forgot to save remap.lua -__-

--- lua/luis/remap.lua
+++ lua/luis/remap.lua
@@ -22,6 +22,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "K", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
