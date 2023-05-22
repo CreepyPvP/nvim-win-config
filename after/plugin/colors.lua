27a3089055d4364d367f281f20d7f6a8cf5ae9e4
Luis Dietl <interruptedThread@gmail.com>
2023-05-22 22:47:05 +0200

    Changed lsp hotkeys + colorscheme
    Signed-off-by: Luis Dietl <interruptedThread@gmail.com>

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencil(color)
-	color = color or "tokyonight-storm"
+	color = color or "tokyonight-night"
 	vim.cmd.colorscheme(color)
 
 	-- vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
