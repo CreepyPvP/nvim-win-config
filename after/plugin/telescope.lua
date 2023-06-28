3278aa9bed0dac07d136522e5bfed6bd64109f85
Luis Dietl <interruptedThread@gmail.com>
2023-06-28 10:51:02 +0200

    Excluded png files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,11 @@
+require('telescope').setup{ 
+  defaults = { 
+    file_ignore_patterns = { 
+      ".png" 
+    }
+  }
+}
+
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
