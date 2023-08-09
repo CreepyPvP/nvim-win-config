ea79748ac13b63b3d857173c20555cbf32c39d65
Luis Dietl <interruptedThread@gmail.com>
2023-08-09 18:24:43 +0200

    Reenabled project conf

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,9 +2,9 @@ function ColorMyPencil(color)
 	color = color or "gruvbox-material"
 	vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalNC", {bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", {bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
 end
 
 ColorMyPencil()
--- after/plugin/projectconf.lua
+++ after/plugin/projectconf.lua
@@ -1,12 +1,13 @@
 -- source ./.vim.lua
 --vim.cmd("source test.lua");
 
--- local config = vim.fs.find(".nvim.lua", {
---     type = "file",
---     upward =  false,
---     path = ".",
--- });
---
--- for  k,v in pairs(config) do
---     vim.cmd("source  " .. v);  
--- end
+local config = vim.fs.find(".nvim.lua", {
+    type = "file",
+    upward =  true,
+    stop = "../..",
+    path = ".",
+});
+
+for  k,v in pairs(config) do
+    vim.cmd("source  " .. v);  
+end
