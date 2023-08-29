7531256009cfcf612d5564d5fa1ac200e9a4b27b
Luis Dietl <interruptedThread@gmail.com>
2023-08-29 21:31:02 +0200

    Fixed bug

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>ga", "<cmd>Git add .<CR>")
 vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>")
-vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR")
+vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
