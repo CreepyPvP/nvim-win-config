5b46446dda3dcadaca89072831714b6be6877e87
Luis Dietl <interruptedThread@gmail.com>
2023-09-16 19:54:25 +0200

    Iam stupid

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencil(color)
-	color = color or "gruvbox-material"
+	color = color or "nordic"
 	vim.cmd.colorscheme(color)
 
     -- vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
