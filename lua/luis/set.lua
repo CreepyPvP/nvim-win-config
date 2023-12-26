c96b48598a78a19aef506154c4e3a1c96cfd540b
Luis Dietl <interruptedThread@gmail.com>
2023-12-26 16:49:01 +0100

    Some more changes

--- lua/luis/set.lua
+++ lua/luis/set.lua
@@ -1,5 +1,5 @@
-vim.opt.relativenumber = true
-vim.opt.number = true
+-- vim.opt.relativenumber = true
+-- vim.opt.number = true
   
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -35,3 +35,5 @@ vim.opt.splitkeep = "topline"
 
 vim.g.netrw_banner = 0 -- Hide banner
 vim.opt.scl = "no"
+
+vim.opt.cindent = true;
